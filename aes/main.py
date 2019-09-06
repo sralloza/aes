eba3b97f0af46926f624f9a29bb7ba8ebf50d107
sralloza <sralloza@gmail.com>
2019-09-06 14:12:02 +0200

    Add version argument

--- aes/main.py
+++ aes/main.py
@@ -2,6 +2,7 @@ from argparse import ArgumentParser
 
 from cryptography.fernet import InvalidToken
 
+from aes import version
 from .files import encrypt_file, decrypt_file
 
 __all__ = ['main']
@@ -9,6 +10,7 @@ __all__ = ['main']
 
 def main():
     parser = ArgumentParser('AES')
+    parser.add_argument('--v', '-version', action='store_true', help='Show version')
     subparsers = parser.add_subparsers(title='commands', dest='command')
 
     encrypt_parser = subparsers.add_parser('encrypt')
@@ -20,6 +22,9 @@ def main():
     options = parser.parse_args()
 
     if options.command is None:
+        if options.v:
+            exit('Version: %r' % version)
+
         parser.error('Invalid use: use decrypt or encrypt')
     elif options.command == 'decrypt':
         try:
