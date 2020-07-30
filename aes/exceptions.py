e2ffa5fd82185be6bbb842f054110b0995ea9983
sralloza <sralloza@gmail.com>
2020-07-30 17:55:47 +0200

    feat: add exception FolderNotFoundError

--- aes/exceptions.py
+++ aes/exceptions.py
@@ -5,5 +5,9 @@ class BaseAESError(Exception):
     """Base AES error."""
 
 
+class FolderNotFoundError(BaseAESError):
+    """Folder not found error"""
+
+
 class IncorrectPasswordError(BaseAESError):
     """Incorrect password error"""
